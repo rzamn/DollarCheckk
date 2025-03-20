677156d3e00c17fd87f83dcb33a03631391bc37e
Ahmed Raza Amaan <aramaan78@gmail.com>
2025-03-20 00:36:30 -0400

    dollarcheck_complete_code.py

--- dollarcheck_complete_code.py
+++ dollarcheck_complete_code.py
@@ -258,10 +258,8 @@ def load_user(user_id):
 app.register_blueprint(auth)
 app.register_blueprint(main)
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
 """
 HTML Templates Structure:
